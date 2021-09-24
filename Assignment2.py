da63eb1345e5965853771937f9793011d0a2f5f4
Srihari <psrihari7@gmail.com>
2021-09-24 17:54:05 +0530

    Add files via upload
    Start
    Assignment2

---
+++ Assignment2.py
@@ -0,0 +1,30 @@
+
+
+import os
+
+
+
+def ventry():
+    os.system("cls")
+    print("DATA SCIENCE SEMINAR\n")
+    print('-'*30)
+    print('\t\t\tMENU')
+    print('-' * 30)
+    print("""
+    1.New Visitor
+    2.Visitor List
+    3.Categorywise Summary
+    0.Exit   
+    """)
+    input("Enter your code 0-3 :")
+
+
+
+def vlist():
+    pass
+def summary():
+    pass
+
+ventry()
+# print("Hello World")
+# print("all is well")
